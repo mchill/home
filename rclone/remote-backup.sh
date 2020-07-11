9cdff4b7603337d2859b4248034ad3bc036283c4
Matthew Hill <matthewchill7@gmail.com>
2020-07-11 11:32:54 -0400

    Run cron in foreground
    Redirect logs from cron jobs to stdout to be collected by Docker.

--- rclone/remote-backup.sh
+++ rclone/remote-backup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 cd /data/server
-getfacl -R . > permissonions.facl
-rclone sync -l --progress --stats-one-line /data/server remote:server
+command -v getfacl || apk add acl
+getfacl -R . > permissions.facl
+rclone sync -lv /data/server remote:server
