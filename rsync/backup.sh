f291bd87cd1e809ed82b725ed4d5df36d0b63bf7
Matthew Hill <matthewchill7@gmail.com>
2020-11-01 01:58:15 -0500

    Migrate backup
    Backup the /server, /server2, and /data directories to my NAS.

--- rsync/backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-cd /backup
-command -v getfacl || apk add acl
-getfacl -R . > permissions.facl
-ssh-keyscan -H nas >> ~/.ssh/known_hosts
-rsync -av --delete-before /backup rsync@nas:/volume1/Server
