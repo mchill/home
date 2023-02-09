4830895a7a004894e287aa1c4f4c2bd95099c370
Matthew Hill <matthewchill7@gmail.com>
2023-02-09 13:40:24 -0500

    Reset cluster
    Believe it or not. This time, Longhorn didn't handle the control
    node restarting well.

--- k8s/infrastructure/apply.sh
+++ k8s/infrastructure/apply.sh
@@ -7,6 +7,7 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
 kubectl apply --server-side -k sealed-secrets
+kubectl wait --for=condition=available --timeout=60s -n sealed-secrets deployment/sealed-secrets-controller
 find ../ -type f -iname secret.yaml -not -path "*/charts/*" -execdir sh -c "cat {} | kubeseal --controller-namespace=sealed-secrets -o yaml > sealed-secret.yaml" \;
 
 kubectl apply --server-side -k cert-manager
@@ -17,5 +18,6 @@ kubectl apply --server-side -k server
 pushd traefik-external && helm upgrade --install --create-namespace -n traefik-external --version 20.8.0 --values values.yaml --post-renderer ../kustomize.sh traefik-external traefik/traefik && popd
 pushd traefik-internal && helm upgrade --install --create-namespace -n traefik-internal --version 20.8.0 --values values.yaml --post-renderer ../kustomize.sh traefik-internal traefik/traefik && popd
 pushd longhorn && helm upgrade --install --create-namespace -n longhorn-system --version 1.4.0 --values values.yaml --post-renderer ../kustomize.sh longhorn longhorn/longhorn && popd
+kubectl apply -n longhorn-system -f longhorn/recurring-job.yaml
 pushd prometheus && helm upgrade --install --create-namespace -n monitoring --version 44.3.0 --values values.yaml --post-renderer ../kustomize.sh prometheus prometheus/kube-prometheus-stack && popd
 pushd loki && helm upgrade --install -n monitoring --create-namespace --version 2.8.9 --values values.yaml --post-renderer ../kustomize.sh loki grafana/loki-stack && popd
