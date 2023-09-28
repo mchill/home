95b114d52415018995c05006cd2749753dc7c886
Matthew Hill <matthewchill7@gmail.com>
2023-09-27 20:38:04 -0400

    Move assets
    Now that libraries are organized by type, keep assets alongside
    their corresponding media.

--- k8s/applications/plex-meta-manager/config/sync.sh
+++ k8s/applications/plex-meta-manager/config/sync.sh
@@ -3,21 +3,21 @@
 BASE_DIR="/data"
 RSYNC_OPTIONS=(-av --prune-empty-dirs --include='*/' --include='poster.*' --include='fanart.*' --include='theme.*' --exclude='*')
 
-rsync ${RSYNC_OPTIONS[@]} $BASE_DIR/movies/ $BASE_DIR/assets/movies
-rsync ${RSYNC_OPTIONS[@]} $BASE_DIR/tv/ $BASE_DIR/assets/tv
-rsync ${RSYNC_OPTIONS[@]} $BASE_DIR/extras/ $BASE_DIR/assets/extras
+rsync ${RSYNC_OPTIONS[@]} $BASE_DIR/movies/sd/ $BASE_DIR/movies/assets
+rsync ${RSYNC_OPTIONS[@]} $BASE_DIR/tv/sd/ $BASE_DIR/tv/assets
+rsync ${RSYNC_OPTIONS[@]} $BASE_DIR/extras/sd/ $BASE_DIR/extras/assets
 
-for d in $BASE_DIR/movies/*/; do
+for d in $BASE_DIR/movies/sd/*/; do
     movie=$(basename "$d")
-    rsync ${RSYNC_OPTIONS[@]} "$BASE_DIR/assets/movies/$movie/" "$d";
+    rsync ${RSYNC_OPTIONS[@]} "$BASE_DIR/movies/assets/$movie/" "$d";
 done
 
-for d in $BASE_DIR/tv/*/; do
+for d in $BASE_DIR/tv/sd/*/; do
     show=$(basename "$d")
-    rsync ${RSYNC_OPTIONS[@]} "$BASE_DIR/assets/tv/$show/" "$d";
+    rsync ${RSYNC_OPTIONS[@]} "$BASE_DIR/tv/assets/$show/" "$d";
 done
 
-for d in $BASE_DIR/extras/*/; do
+for d in $BASE_DIR/extras/sd/*/; do
     extra=$(basename "$d")
-    rsync ${RSYNC_OPTIONS[@]} "$BASE_DIR/assets/extras/$extra/" "$d";
+    rsync ${RSYNC_OPTIONS[@]} "$BASE_DIR/extras/assets/$extra/" "$d";
 done
