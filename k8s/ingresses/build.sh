51c838beb4ec820e04cef1147681b06498930227
Matthew Hill <matthew.hill@voith.com>
2025-04-02 01:30:05 -0400

    Restructure network
    Move static IP range and organize static IP assignments.

--- k8s/ingresses/build.sh
+++ k8s/ingresses/build.sh
@@ -4,6 +4,8 @@ kubectl kustomize kvm && echo "---"
 kubectl kustomize nas && echo "---"
 kubectl kustomize pdu && echo "---"
 kubectl kustomize pihole && echo "---"
+kubectl kustomize proxmox && echo "---"
+kubectl kustomize rancher && echo "---"
 kubectl kustomize unifi && echo "---"
 
 kubectl kustomize remote/nas && echo "---"
