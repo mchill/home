bebb1a14fa55ee60581e65f7fecfc3a74c68d8c0
Matthew Hill <matthew.hill@voith.com>
2025-04-13 12:34:08 -0400

    Configure event-exporter
    Send all k8s events to loki. Trigger a Home Assistant automation
    when jobs fail.

--- k8s/infrastructure/apply.sh
+++ k8s/infrastructure/apply.sh
@@ -6,6 +6,7 @@ helm repo add traefik https://helm.traefik.io/traefik
 helm repo add longhorn https://charts.longhorn.io
 helm repo add prometheus https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 kubectl apply --server-side -k sealed-secrets
@@ -18,7 +19,10 @@ kubectl apply --server-side -k server
 
 pushd traefik-external && helm upgrade --install --create-namespace -n traefik-external --version 27.0.2 --values values.yaml --post-renderer ../kustomize.sh traefik-external traefik/traefik && popd
 pushd traefik-internal && helm upgrade --install --create-namespace -n traefik-internal --version 27.0.2 --values values.yaml --post-renderer ../kustomize.sh traefik-internal traefik/traefik && popd
+
 pushd longhorn && helm upgrade --install --create-namespace -n longhorn-system --version 1.7.2 --values values.yaml --post-renderer ../kustomize.sh longhorn longhorn/longhorn && popd
 kubectl apply -n longhorn-system -f longhorn/recurring-job.yaml
+
 pushd prometheus && helm upgrade --install --create-namespace -n monitoring --version 66.2.1 --values values.yaml --post-renderer ../kustomize.sh prometheus prometheus/kube-prometheus-stack && popd
-pushd loki && helm upgrade --install -n monitoring --create-namespace --version 2.10.2 --values values.yaml --post-renderer ../kustomize.sh loki grafana/loki-stack && popd
+pushd loki && helm upgrade --install -n monitoring --version 2.10.2 --values values.yaml --post-renderer ../kustomize.sh loki grafana/loki-stack && popd
+push event-exporter && helm upgrade --install -n monitoring --version 3.4.5 --values values.yaml --post-renderer ../kustomize.sh event-exporter bitnami/kubernetes-event-exporter && popd
