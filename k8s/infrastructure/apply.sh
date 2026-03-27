27b3c7526e4488575c030e924c1842e2b5176b68
Matthew Hill <matthew.hill@voith.com>
2026-03-27 03:21:34 -0400

    Update monitoring stack
    Update to the latest kube-prometheus-stack. Move from deprecated
    loki-stack chart to separate loki and alloy charts. Alloy eats
    more file watchers than Promtail, apparently, so increase that.

--- k8s/infrastructure/apply.sh
+++ k8s/infrastructure/apply.sh
@@ -2,12 +2,13 @@
 
 curl -skSL https://raw.githubusercontent.com/kubernetes-csi/csi-driver-smb/v1.16.0/deploy/install-driver.sh | bash -s v1.16.0 --
 
-helm repo add ceph-csi https://ceph.github.io/csi-charts
 helm repo add traefik https://helm.traefik.io/traefik
+helm repo add ceph-csi https://ceph.github.io/csi-charts
+helm repo add intel https://intel.github.io/helm-charts/
 helm repo add prometheus https://prometheus-community.github.io/helm-charts
+helm repo add grafana-community https://grafana-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo add intel https://intel.github.io/helm-charts/
 helm repo update
 
 kubectl apply --server-side -k sealed-secrets
@@ -21,7 +22,9 @@ kubectl apply --server-side -k kube-vip
 
 pushd traefik && helm upgrade --install --create-namespace -n traefik --version 27.0.2 --values values.yaml --post-renderer ../kustomize.sh traefik traefik/traefik && popd
 pushd ceph && helm upgrade --install --create-namespace -n ceph-csi-rbd --version 3.16.0 --values values.yaml --post-renderer ../kustomize.sh ceph-csi-rbd ceph-csi/ceph-csi-rbd && popd
-pushd prometheus && helm upgrade --install --create-namespace -n monitoring --version 66.2.1 --values values.yaml --post-renderer ../kustomize.sh prometheus prometheus/kube-prometheus-stack && popd
-pushd loki && helm upgrade --install -n monitoring --version 2.10.2 --values values.yaml --post-renderer ../kustomize.sh loki grafana/loki-stack && popd
-pushd event-exporter && helm upgrade --install -n monitoring --version 3.6.3 --values values.yaml --post-renderer ../kustomize.sh event-exporter bitnami/kubernetes-event-exporter && popd
 pushd intel && helm upgrade --install --create-namespace -n inteldeviceplugins-system --version 0.35.0 --post-renderer ../kustomize.sh device-plugins-operator intel/intel-device-plugins-operator && popd
+
+pushd prometheus && helm upgrade --install --create-namespace -n monitoring --version 82.15.0 --values values.yaml --post-renderer ../kustomize.sh prometheus prometheus/kube-prometheus-stack && popd
+pushd loki && helm upgrade --install --create-namespace -n monitoring --version 9.3.1 --values values.yaml loki grafana-community/loki && popd
+pushd alloy && helm upgrade --install --create-namespace -n monitoring --version 1.6.2 --values values.yaml alloy grafana/alloy && popd
+pushd event-exporter && helm upgrade --install --create-namespace -n monitoring --version 3.6.3 --values values.yaml --post-renderer ../kustomize.sh event-exporter bitnami/kubernetes-event-exporter && popd
--- k8s/infrastructure/kustomize.sh
+++ k8s/infrastructure/kustomize.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+trap 'rm -f helm.yaml' EXIT
+
 cat <&0 > helm.yaml
-kubectl kustomize
-rm helm.yaml
+kubectl kustomize --enable-helm --load-restrictor=LoadRestrictionsNone
