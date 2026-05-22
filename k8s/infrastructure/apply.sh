08f46e774c5a8b604daaafcd5dfb3e1355429dd7
Matthew Hill <matthew.hill@voith.com>
2026-05-22 11:03:09 +0200

    Upgrade to helm 4
    Convert the post-renderer to a plugin. Move charts to helm folder.

--- k8s/infrastructure/apply.sh
+++ k8s/infrastructure/apply.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+HELM_PLUGINS=../../helm/plugins
+
 curl -skSL https://raw.githubusercontent.com/kubernetes-csi/csi-driver-smb/v1.16.0/deploy/install-driver.sh | bash -s v1.16.0 --
 
 helm repo add traefik https://helm.traefik.io/traefik
 helm repo add ceph-csi https://ceph.github.io/csi-charts
-helm repo add intel https://intel.github.io/helm-charts/
+helm repo add intel https://intel.github.io/helm-charts
+helm repo add argo https://argoproj.github.io/argo-helm
 helm repo add prometheus https://prometheus-community.github.io/helm-charts
 helm repo add grafana-community https://grafana-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
@@ -20,11 +23,11 @@ kubectl apply --server-side -k metallb
 kubectl apply --server-side -k server
 kubectl apply --server-side -k kube-vip
 
-pushd traefik && helm upgrade --install --create-namespace -n traefik --version 27.0.2 --values values.yaml --post-renderer ../kustomize.sh traefik traefik/traefik && popd
-pushd ceph && helm upgrade --install --create-namespace -n ceph-csi-rbd --version 3.16.0 --values values.yaml --post-renderer ../kustomize.sh ceph-csi-rbd ceph-csi/ceph-csi-rbd && popd
-pushd intel && helm upgrade --install --create-namespace -n inteldeviceplugins-system --version 0.35.0 --post-renderer ../kustomize.sh device-plugins-operator intel/intel-device-plugins-operator && popd
+pushd traefik && helm upgrade --install --create-namespace -n traefik --version 27.0.2 --values values.yaml --post-renderer kustomize-renderer traefik traefik/traefik && popd
+pushd ceph && helm upgrade --install --create-namespace -n ceph-csi-rbd --version 3.16.0 --values values.yaml --post-renderer kustomize-renderer ceph-csi-rbd ceph-csi/ceph-csi-rbd && popd
+pushd intel && helm upgrade --install --create-namespace -n inteldeviceplugins-system --version 0.35.0 --post-renderer kustomize-renderer device-plugins-operator intel/intel-device-plugins-operator && popd
 
-pushd prometheus && helm upgrade --install --create-namespace -n monitoring --version 82.15.0 --values values.yaml --post-renderer ../kustomize.sh prometheus prometheus/kube-prometheus-stack && popd
+pushd prometheus && helm upgrade --install --create-namespace -n monitoring --version 82.15.0 --values values.yaml --post-renderer kustomize-renderer prometheus prometheus/kube-prometheus-stack && popd
 pushd loki && helm upgrade --install --create-namespace -n monitoring --version 9.3.1 --values values.yaml loki grafana-community/loki && popd
 pushd alloy && helm upgrade --install --create-namespace -n monitoring --version 1.6.2 --values values.yaml alloy grafana/alloy && popd
-pushd event-exporter && helm upgrade --install --create-namespace -n monitoring --version 3.6.3 --values values.yaml --post-renderer ../kustomize.sh event-exporter bitnami/kubernetes-event-exporter && popd
+pushd event-exporter && helm upgrade --install --create-namespace -n monitoring --version 3.6.3 --values values.yaml --post-renderer kustomize-renderer event-exporter bitnami/kubernetes-event-exporter && popd
